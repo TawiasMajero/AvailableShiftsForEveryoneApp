cd79b88f16484303e2c7d985fbf11a6764c86650
Denis Mendelev <tawias86@gmail.com>
2025-09-20 20:56:04 +0300

    Создание AppNavigator и базовых DetailsScreen, ListScreen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,28 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import ListScreen from '../screens/ListScreen';
+import DetailsScreen from '../screens/DetailsScreen';
 
-const AppNavigator = () => {
-  return <div></div>;
-};
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="List">
+        <Stack.Screen
+          name="List"
+          component={ListScreen}
+          options={{ title: 'Доступные смены' }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: 'Детали смены' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default AppNavigator;
